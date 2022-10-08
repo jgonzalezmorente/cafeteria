ea3d834800ec3174fb4a4d7330e5b92df340d19b
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-10-08 18:33:24 +0200

    Cafetería finalizado

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const { src, dest, watch, series, parallel } = require( 'gulp' );
+
+// CSS y SASS
+const sass = require( 'gulp-sass' )( require( 'sass' ) );
+const postcss = require( 'gulp-postcss' );
+const autoprefixer = require( 'autoprefixer' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+const cssnano = require( 'cssnano' );
+
+// Imágenes
+const imagemin = require( 'gulp-imagemin' );
+const webp = require( 'gulp-webp' );
+const avif = require( 'gulp-avif' );
+
+
+function css( done ) {
+    // compilar sass
+    // pasos: 1 - identificar archivo, 2 - compilarla, 3 - guardar .css
+    src( 'src/scss/app.scss' )
+        .pipe( sourcemaps.init() )
+        .pipe( sass() )
+        .pipe( postcss( [ autoprefixer(), cssnano() ] ) )
+        .pipe( sourcemaps.write( '.' ) )
+        .pipe( dest( 'build/css' ) );
+
+    done();
+}
+
+
+function imagenes() {
+    return src( 'src/img/**/*' )
+        .pipe( imagemin( { optimizationLevel: 3 }) )        
+        .pipe( dest( 'build/img' ) );
+}
+
+function versionWebp() {
+    const opciones = {
+        quality: 50
+    }
+    return src( 'src/img/**/*.{png,jpg}')
+        .pipe( webp( opciones ) )
+        .pipe( dest( 'build/img' ) );
+}
+
+
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src( 'src/img/**/*.{png,jpg}')
+        .pipe( avif( opciones ) )
+        .pipe( dest( 'build/img') );
+}
+
+
+function dev() {
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/img/**/*', imagenes );
+}
+
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev );
+
+// series - Se inicia una taream, y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician la mismo tiempo
